c3eec26ec1d4a4be376ae60b2606d081410d501d
kayang0000 <18410422493@163.com>
2019-03-25 09:27:23 +0800

    NO.2 version rabbitmq commit

--- src/main/java/com/sl/consumer/RecvMQ.java
+++ src/main/java/com/sl/consumer/RecvMQ.java
@@ -6,27 +6,46 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class RecvMQ {
-    private final static String QUEUE_NAME = "Hello";
+    private final static String QUEUE = "helloworld";
 
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
+        // 设置RabbitMQ 躲在的服务器的ip 和端口
         factory.setHost("127.0.0.1");
         factory.setPort(5672);
-        factory.setUsername("guest");
-        factory.setPassword("guest");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println("[*]Waiting for messages. To exit press CTRL+C");
-        Consumer consumer = new DefaultConsumer(channel) {
+        // 声明队列
+        channel.queueDeclare(QUEUE, true, false, false, null);
+        // 定义消费方法
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            /**
+             * 消费者接受消息调用方法
+             * param1:消费者的标签，在channel.basicConsume() 去指定
+             * param2:消费包的内容，可从中获取消息id，消息routingKey，交换机，消息和重传标志（收到消息失败后是否需要重新发送）
+             * param3:-
+             * param4:-
+             */
             @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
-                                       byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
-                System.out.println("[x]Received " + message);
-                //super.handleDelivery(consumerTag, envelope, properties, body);
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                // 交换机
+                String exchange = envelope.getExchange();
+                // 路由key
+                String routingKey = envelope.getRoutingKey();
+                // 消息id
+                long deliveryTag = envelope.getDeliveryTag();
+                // 消息内容
+                String msg = new String(body, "UTF-8");
+                System.out.println("receive message.." + msg);
             }
         };
-        channel.basicConsume(QUEUE_NAME, true, consumer);
+        /**
+         * 监听队列
+         * param1:队列名称
+         * param2:是否自动回复，设置为true为标识消息家受到自动向mq回复接收到了，mq接收到回复会删除消息，设置为false则需要手动回复
+         * param3:消费消息的方法，消费者接收到消息后调用此方法
+         */
+        channel.basicConsume(QUEUE, true, consumer);
     }
 }
--- src/main/java/com/sl/provider/SendMQ.java
+++ src/main/java/com/sl/provider/SendMQ.java
@@ -7,23 +7,62 @@ import com.rabbitmq.client.ConnectionFactory;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * <p>第二版Rabbitmq 测试
+ *
+ * @author kqyang
+ * @version 1.0
+ * @date 2019/3/24 11:40
+ */
 public class SendMQ {
-    private final static String QUEUE_NAME = "Hello";
+    private final static String QUEUE = "helloworld";
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("127.0.0.1");
-        // 默认端口号
-        factory.setPort(5672);
-        factory.setUsername("guest");
-        factory.setPassword("guest");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        String message = "Hello world";
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-        System.out.println("[x]Sent " + message);
-        channel.close();
-        connection.close();
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("127.0.0.1");
+            factory.setPort(5672);
+            factory.setUsername("guest");
+            factory.setPassword("guest");
+            // Rabbitmq 默认虚拟机名称为"/"，虚拟机相当于一个独立的mq服务
+            factory.setVirtualHost("/");
+            // 创建与RabbitMQ 服务的TCP连接
+            connection = factory.newConnection();
+            // 创建与Exchange的通道，每个连接可以创建多个通道，每个通道代表一个会话任务
+            channel = connection.createChannel();
+            /**
+             * 声明队列，如果Rabbit 中没有此队列将自动创建
+             * param1:队列名称
+             * param2:是否持久化
+             * param3:队列是否独占此连接
+             * param4:队列不再使用时是否自动删除此队列
+             * param5:队列参数
+             */
+            channel.queueDeclare(QUEUE, true, false, false, null);
+            String message = "helloworld小明 " + System.currentTimeMillis();
+            /**
+             * 消息发布方法
+             * param1:Exchange 的名称，如果没有指定，则使用Default Exchange
+             * param2:routingKey，消息的路由key，是用于Exchange（交换机）将消息转发到指定的消息队列
+             * param3:消息包含的属性
+             * param4:消息体
+             *
+             * 这里没有指定交换机，消息将发送给默认的交换机，每个队列也会绑定到哪个默认的交换机，但是不能显示绑定或解除绑定
+             * 默认的交换机，routingKey 等于队列名称
+             */
+            channel.basicPublish("", QUEUE, null, message.getBytes());
+            System.out.println("Send Message is: '" + message + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != channel) {
+                channel.close();
+            }
+            if (null != connection) {
+                connection.close();
+            }
+        }
     }
 }
